9e183ffa9594febd5d434dadd524b523653c5aef
Mikko P <mikko.perala@tuni.fi>
2023-06-09 15:51:47 +0300

    Add reset and resize grid buttons

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-function initBoxes() {
+function initBoxes(squares = 16) {
     const container = document.querySelector("#container");
+    container.innerHTML = "";
     
-    const row = 16;
-    const col = 16;
-    
-    for (let i = 0; i < row; i++) {
+    for (let i = 0; i < squares; i++) {
         let boxRow = document.createElement('div');
         boxRow.classList.add('rowOfBoxes');
-        for (let j = 0; j < col; j++) {
+        for (let j = 0; j < squares; j++) {
             let divBox = document.createElement('div');
             divBox.classList.add('textBox');
             boxRow.appendChild(divBox);
         }
         container.appendChild(boxRow);
     }
+    initBoxListeners();
 }
 
-function initListeners() {
+function initBoxListeners() {
     const boxes = document.querySelectorAll('div.textBox');
     boxes.forEach(box => {
         box.addEventListener('mouseover', (e) => {
@@ -25,7 +24,30 @@ function initListeners() {
     }); 
 }
 
+function resetGrid() {
+    const boxes = document.querySelectorAll('.painted');
+    boxes.forEach(box => {
+        box.classList.remove('painted');
+    })
+}
+
+function initButtons() {
+    const resetBtn = document.querySelector('#resetButton');
+    const gridBtn = document.querySelector('#setGridButton');
+    
+    resetBtn.addEventListener('click', () => {
+        resetGrid();
+    });
+    
+    gridBtn.addEventListener('click', () => {
+        let setSquares = prompt("Set number of squares in grid (max 100):");
+        if (setSquares > 100) {
+            alert("Error: Number of squares must be between 1-100");
+        } else {
+            initBoxes(setSquares);
+        }
+    });
+}
 
 initBoxes();
-initListeners();
-
+initButtons();
